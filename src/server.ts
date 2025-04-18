40d21de029d4048c719b5cb916f5c11bde584c1b
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-04-18 23:21:51 +0530

    project steup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express ,{Application} from "express";
+
+class Server{
+    private app : Application
+
+    constructor(){
+        this.app = express()
+    }
+
+    public listenServer(){
+        const port = 5000;
+        
+        this.app.listen(port , ()=>{
+            console.log(`connected to server with port ${port}`)
+        })
+    }
+}
+export default Server
