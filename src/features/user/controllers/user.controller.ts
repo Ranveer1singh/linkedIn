a8e81de576fc0f42eeae0d7f7cbb43bae72648c8
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-04-21 21:43:37 +0530

    changes in prisma and add user

--- src/features/user/controllers/user.controller.ts
+++ src/features/user/controllers/user.controller.ts
@@ -1,14 +1,20 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express'
+import prisma from '~/prisma'
 
 class UserController {
-    public getAll(req : Request , res: Response, next : NextFunction){
-        res.status(200).json({
-            message : "get all user"
-        })
-
-
-
-        
-    }
+  public async getAll(req: Request, res: Response, next: NextFunction) {
+    await prisma.user.create({
+      data: {
+        name: 'test',
+        email: 'test@mail.com',
+        password: 'test123',
+        role: 'User',
+        status: true
+      }
+    })
+    res.status(200).json({
+        data :[]
+    })
+  }
 }
-export const userController : UserController = new UserController(); 
+export const userController: UserController = new UserController()
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import Server from "./server"
 class JobApplication {
     public run():void{
         const server = new Server;
-
         server.start()
     }
 }
 const jobApplication: JobApplication = new JobApplication();
- 
+jobApplication.run()
